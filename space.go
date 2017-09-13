7b26b539de52231ccedc9cb2db3ae0a28a1c4d5a
Hiroaki Nakamura <hnakamur@gmail.com>
2017-09-14 08:58:10 +0900

    Optimize IsSpace
    $ benchstat space.bench space2.bench
    name       old time/op  new time/op  delta
    IsSpace-2   333ns ± 0%   167ns ± 0%  -49.94%  (p=0.000 n=6+10)
    
    $ go test -count=10 -run=NONE -bench=. | tee space2.bench
    goos: linux
    goarch: amd64
    pkg: github.com/hnakamur/asciibytes
    BenchmarkIsSpace-2      10000000               166 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               166 ns/op
    BenchmarkIsSpace-2      10000000               166 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    BenchmarkIsSpace-2      10000000               167 ns/op
    PASS
    ok      github.com/hnakamur/asciibytes  18.502s

--- space.go
+++ space.go
@@ -1,12 +1,9 @@
 package asciibytes
 
+var asciiSpace = [256]bool{'\t': true, '\n': true, '\v': true, '\f': true, '\r': true, ' ': true}
+
 // IsSpace returns true if b is one of '\t', '\n', '\v', '\f', '\r', or ' '.
 // It returns false otherwise.
 func IsSpace(b byte) bool {
-	switch b {
-	case '\t', '\n', '\v', '\f', '\r', ' ':
-		return true
-	default:
-		return false
-	}
+	return asciiSpace[b]
 }
