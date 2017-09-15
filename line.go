483c613b45534db32c4669c76960d28b64bb57e1
Hiroaki Nakamura <hnakamur@gmail.com>
2017-09-15 16:40:30 +0900

    Make GetLine document clearer

--- line.go
+++ line.go
@@ -1,6 +1,6 @@
 package ascii
 
-// GetLine returns a line including the last '\n' byte.
+// GetLine searches the first '\n' and returns a line including '\n'.
 func GetLine(buf []byte) []byte {
 	for i, b := range buf {
 		if b == '\n' {
